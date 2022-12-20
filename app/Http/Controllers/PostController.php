26dd8f29bd75e14198fd9d8774db716029602b6c
yanhashi521 <hashiryu0521@gmail.com>
2022-12-20 17:28:10 +0900

    paginate実装まで

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用．
     {
-	    return view('posts/index')->with(['posts' => $post->get()]);
+	    return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
          //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
 }
