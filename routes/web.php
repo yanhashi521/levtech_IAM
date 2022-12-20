7f69093d361ae6ad2eb542e0ab465064f94c1a70
yanhashi521 <hashiryu0521@gmail.com>
2022-12-20 11:57:08 +0900

    add基礎＆初期設定

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -9,10 +12,7 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+| contai
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts',[PostController::class, 'index']);
