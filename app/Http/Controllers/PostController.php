a366457f206273756196f9f2e36409bbac02279f
yanhashi521 <hashiryu0521@gmail.com>
2023-01-01 17:52:59 +0900

    記事投稿画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,4 +19,9 @@ class PostController extends Controller
         return view('posts/show')->with(['post' => $post]);
         //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
+    
+    public function create()
+    {
+        return view('posts/create');
+    }
 }
